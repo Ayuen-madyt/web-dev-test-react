c6fd40f8ae020dbde98c940b917d58ffae5a8cce
Ayuen-madyt <madytiun@gmail.com>
2022-01-17 21:02:28 +0300

    deploying

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { useSelector } from 'react-redux'
 
 function Drivers() {
     const drivers = useSelector(state=>state.Drivers);
+    const[input, setInput] = useState('');
+    const[searchResults, setSearchResults] = useState([]);
+
+    const searchHandler = (e)=>{
+        setInput(e.target.value);
+        if(drivers && input!==""){
+            const filtereddrivers = drivers.filter(driver=>{
+                return Object.values(driver).join(" ").toLowerCase().includes(input.toLowerCase())
+            });
+            setSearchResults(filtereddrivers);
+        }
+        if(input===""){
+            setSearchResults([]);
+        }
+    }
     return (
         <div style={{marginTop:"20px"}}>
             <h3>Drivers page</h3>
+            <div id="pol" className="input-group searchbar">
+                <div className="form-outline searchinput">
+                    <input value={input} onChange={searchHandler} id="search-focus" placeholder="Search..." type="search" className="form-control" />
+                    {/* <button onClick={closeSearch} className="btn bg-dark text-light searchbutton">close</button> */}
+                </div>
+            </div>
 
             <table class="table">
             <thead>
@@ -16,14 +37,32 @@ function Drivers() {
                 </tr>
             </thead>
             <tbody>
-            {
+
+            {searchResults.length>0?
+                        searchResults.map(driver=>(
+                            <tr key={driver.id}>
+                            <td>{driver.id}</td>
+                            <td>{driver.first_name}</td>
+                            <td>{driver.last_name}</td>
+                        </tr>
+                        )) :
+                        
+                        drivers.map(driver=>(
+                            <tr key={driver.id}>
+                            <td>{driver.id}</td>
+                            <td>{driver.first_name}</td>
+                            <td>{driver.last_name}</td>
+                        </tr>
+                        ))
+                     }
+            {/* {
            drivers.map(driver=>(
                 <tr key={driver.id}>
                 <td>{driver.id}</td>
                 <td>{driver.first_name}</td>
                 <td>{driver.last_name}</td>
             </tr>
-            ))}
+            ))} */}
             </tbody>
             </table>
         </div>
--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from 'react-redux'
 
 function Hotels() {
     const hotels = useSelector(state=>state.Hotels);
+
+    const[input, setInput] = useState('');
+    const[searchResults, setSearchResults] = useState([]);
+
+    const searchHandler = (e)=>{
+        setInput(e.target.value);
+        if(hotels && input!==""){
+            const filteredHotels = hotels.filter(hotel=>{
+                return Object.values(hotel).join(" ").toLowerCase().includes(input.toLowerCase())
+            });
+            setSearchResults(filteredHotels);
+        }
+        if(input===""){
+            setSearchResults([]);
+        }
+    }
+
     return (
         <div style={{marginTop:"20px"}}>
             <h3>Hotels List</h3>
+            <div id="pol" className="input-group searchbar">
+                <div className="form-outline searchinput">
+                    <input value={input} onChange={searchHandler} id="search-focus" placeholder="Search..." type="search" className="form-control" />
+                    {/* <button onClick={closeSearch} className="btn bg-dark text-light searchbutton">close</button> */}
+                </div>
+            </div>
 
             <table class="table">
             <thead>
@@ -16,14 +39,24 @@ function Hotels() {
                 </tr>
             </thead>
             <tbody>
-            {
-           hotels.map(hotel=>(
-                <tr key={hotel.id}>
-                <td>{hotel.id}</td>
-                <td>{hotel.name}</td>
-                <td>{hotel.address}</td>
-            </tr>
-            ))}
+
+            {searchResults.length>0?
+                        searchResults.map(hotel=>(
+                            <tr key={hotel.id}>
+                            <td>{hotel.id}</td>
+                            <td>{hotel.name}</td>
+                            <td>{hotel.address}</td>
+                        </tr>
+                        )) :
+                        
+                        hotels.map(hotel=>(
+                            <tr key={hotel.id}>
+                            <td>{hotel.id}</td>
+                            <td>{hotel.name}</td>
+                            <td>{hotel.address}</td>
+                        </tr>
+                        ))
+                     }
             </tbody>
             </table>
         </div>
